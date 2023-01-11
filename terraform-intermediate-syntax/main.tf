e26d9d642462607b3f031ccad4387529cb4747cd
Maggie Walsh <miggieholland@gmail.com>
2023-01-11 14:31:50 +0000

    Terraform branch tutorial

---
+++ terraform-intermediate-syntax/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  shared_credentials_files = ["~/.aws/credentials"]
+  region                   = "eu-west-2"
+  alias                    = "aws-uk"
+}
+
+variable "ami-uk" {
+  description = "machine image uk"
+  default     = "ami-035469b606478d63d"
+}
+
+variable "type" {
+  default = "t2.micro"
+}
+
+variable "zone" {
+  description = "map of availability zones for eu-west-2"
+  default = {
+    1 = "eu-west-2a"
+    2 = "eu-west-2b"
+  }
+}
+
+resource "aws_instance" "example" {
+  provider          = "aws.aws-uk"
+  for_each          = var.zone
+  availability_zone = each.value
+  ami               = var.ami-uk
+  instance_type     = var.type
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  timeouts {
+    create = "5m"
+    delete = "2h"
+  }
+}
